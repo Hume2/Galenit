2cd9fee0f6878a8bcef6dcf9c68b5aa97eff44fe
Hume2 <teratux.mail@gmail.com>
2017-05-06 11:44:37 +0200

    vybarvení

--- graphics/voxel.cpp
+++ graphics/voxel.cpp
@@ -8,13 +8,16 @@ namespace voxel {
     glScalef((n & 1) ? 1.0f : -1.0f, (n & 2) ? 1.0f : -1.0f, (n & 4) ? 1.0f : -1.0f);
 
     glBegin(GL_TRIANGLE_FAN);
-    glVertex3f( 0.25f, 0.25f, 0.25f);
-    glVertex3f( 0.25f, 0.00f, 0.50f);
-    glVertex3f( 0.50f, 0.00f, 0.25f);
-    glVertex3f( 0.50f, 0.25f, 0.00f);
-    glVertex3f( 0.25f, 0.50f, 0.00f);
-    glVertex3f( 0.00f, 0.50f, 0.25f);
-    glVertex3f( 0.00f, 0.25f, 0.50f);
+    {
+      glVertex3f( 0.25f, 0.25f, 0.25f);
+      glVertex3f( 0.25f, 0.00f, 0.50f);
+      glVertex3f( 0.50f, 0.00f, 0.25f);
+      glVertex3f( 0.50f, 0.25f, 0.00f);
+      glVertex3f( 0.25f, 0.50f, 0.00f);
+      glVertex3f( 0.00f, 0.50f, 0.25f);
+      glVertex3f( 0.00f, 0.25f, 0.50f);
+      glVertex3f( 0.25f, 0.00f, 0.50f);
+    }
     glEnd();
 
     glScalef((n & 1) ? 1.0f : -1.0f, (n & 2) ? 1.0f : -1.0f, (n & 4) ? 1.0f : -1.0f);
@@ -57,9 +60,11 @@ namespace voxel {
 
   void drawToe() {
     for (int i = 7; i >= 0; i--) {
+      glColor3f(1.0f, 0.0f, 1.0f/7*i);
       drawToeHexagon(i);
     }
     for (int i = 5; i >= 0; i--) {
+      glColor3f(0.0f, 1.0f, 1.0f/5*i);
       drawToeQuad(i);
     }
   }
