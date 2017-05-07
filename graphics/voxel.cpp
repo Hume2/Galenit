674c5e5bac7ae05a4197dc259b9cd7178fc45984
Hume2 <teratux.mail@gmail.com>
2017-05-07 13:37:21 +0200

    texturování

--- graphics/voxel.cpp
+++ graphics/voxel.cpp
@@ -7,14 +7,14 @@ namespace voxel {
   void drawToeHexagon(char n, const ToeColour c) {
     glScalef((n & 1) ? -1.0f : 1.0f, (n & 2) ? -1.0f : 1.0f, (n & 4) ? -1.0f : 1.0f);
 
-    const float v[7][3] = {
-      { 0.25f, 0.25f, 0.25f},
-      { 0.25f, 0.00f, 0.50f},
-      { 0.50f, 0.00f, 0.25f},
-      { 0.50f, 0.25f, 0.00f},
-      { 0.25f, 0.50f, 0.00f},
-      { 0.00f, 0.50f, 0.25f},
-      { 0.00f, 0.25f, 0.50f}
+    const float v[7][5] = {
+      { 0.25f, 0.25f, 0.25f, 0.5f, 0.5f},
+      { 0.25f, 0.00f, 0.50f, 0.0f, 1.0f},
+      { 0.50f, 0.00f, 0.25f, 1.0f, 1.0f},
+      { 0.50f, 0.25f, 0.00f, 1.0f, 0.0f},
+      { 0.25f, 0.50f, 0.00f, 0.0f, 0.0f},
+      { 0.00f, 0.50f, 0.25f, 0.0f, 1.0f},
+      { 0.00f, 0.25f, 0.50f, 1.0f, 1.0f}
     };
 
     glBegin(GL_TRIANGLE_FAN);
@@ -22,17 +22,22 @@ namespace voxel {
     if ((n & 1) != (n & 2) != (n & 4)) {
       for (int i = 0; i < 7; i++) {
         c.hexagons[n][i].use();
+        glTexCoord2f(v[i][3], v[i][4]);
         glVertex3f( v[i][0], v[i][1], v[i][2]);
       }
       c.hexagons[n][1].use();
+      glTexCoord2f(v[1][3], v[1][4]);
       glVertex3f( v[1][0], v[1][1], v[1][2]);
     } else {
       c.hexagons[n][0].use();
+      glTexCoord2f(v[0][3], v[0][4]);
       glVertex3f( v[0][0], v[0][1], v[0][2]);
       c.hexagons[n][1].use();
+      glTexCoord2f(v[1][3], v[1][4]);
       glVertex3f( v[1][0], v[1][1], v[1][2]);
       for (int i = 6; i > 0; i--) {
         c.hexagons[n][i].use();
+        glTexCoord2f(v[i][3], v[i][4]);
         glVertex3f( v[i][0], v[i][1], v[i][2]);
       }
     }
@@ -46,11 +51,11 @@ namespace voxel {
       glScalef(-1.0f, -1.0f, -1.0f);
     }
 
-    const float v[4][2] = {
-      { 0.25f, 0.00f},
-      { 0.00f, 0.25f},
-      {-0.25f,-0.00f},
-      {-0.00f,-0.25f},
+    const float v[4][4] = {
+      { 0.25f, 0.00f, 0.0f, 0.0f},
+      { 0.00f, 0.25f, 0.0f, 1.0f},
+      {-0.25f,-0.00f, 1.0f, 1.0f},
+      {-0.00f,-0.25f, 1.0f, 0.0f},
     };
     const float z = 0.50f;
 
@@ -60,18 +65,21 @@ namespace voxel {
         case 0:
           for (int i = 0; i < 4; i++) {
             c.quads[n][i].use();
+            glTexCoord2f(v[i][2], v[i][3]);
             glVertex3f( v[i][0], v[i][1], z);
           }
           break;
         case 1:
           for (int i = 0; i < 4; i++) {
             c.quads[n][i].use();
+            glTexCoord2f(v[i][2], v[i][3]);
             glVertex3f( v[i][0], z, v[i][1]);
           }
           break;
         case 2:
           for (int i = 0; i < 4; i++) {
             c.quads[n][i].use();
+            glTexCoord2f(v[i][2], v[i][3]);
             glVertex3f(z, v[i][0], v[i][1]);
           }
           break;
@@ -83,18 +91,21 @@ namespace voxel {
         case 0:
           for (int i = 3; i >= 0; i--) {
             c.quads[n][i].use();
+            glTexCoord2f(v[i][2], v[i][3]);
             glVertex3f( v[i][0], v[i][1], z);
           }
           break;
         case 1:
           for (int i = 3; i >= 0; i--) {
             c.quads[n][i].use();
+            glTexCoord2f(v[i][2], v[i][3]);
             glVertex3f( v[i][0], z, v[i][1]);
           }
           break;
         case 2:
           for (int i = 3; i >= 0; i--) {
             c.quads[n][i].use();
+            glTexCoord2f(v[i][2], v[i][3]);
             glVertex3f(z, v[i][0], v[i][1]);
           }
           break;
--- main.cpp
+++ main.cpp
@@ -8,10 +8,13 @@
 //
 #include <ctime>
 #include <random>
+#include <string>
 
 #include <GL/glut.h>    // Header File For The GLUT Library 
 #include <GL/gl.h>	// Header File For The OpenGL32 Library
 #include <GL/glu.h>	// Header File For The GLu32 Library
+//#include <GL/glaux.h>
+#include <stdlib.h>     // Header file for malloc/free.
 #include <unistd.h>     // Header file for sleeping.
 
 #include "main.h"
@@ -23,18 +26,139 @@ ToeColour clr(Colour(1, 1, 1), Colour(0, 1, 0));
 Chunk ch;
 float rot = 60;
 
+GLuint texture[1];
+
 /* ascii code for the escape key */
 #define ESCAPE 27
 
 /* The number of our GLUT window */
 int window; 
 
+/* Image type - contains height, width, and data */
+struct Image {
+    unsigned long sizeX;
+    unsigned long sizeY;
+    char *data;
+};
+typedef struct Image Image;
+
+// quick and dirty bitmap loader...for 24 bit bitmaps with 1 plane only.
+// See http://www.dcs.ed.ac.uk/~mxr/gfx/2d/BMP.txt for more info.
+int ImageLoad(std::string filename, Image *image) {
+  FILE *file;
+  unsigned long size;                 // size of the image in bytes.
+  unsigned long i;                    // standard counter.
+  unsigned short int planes;          // number of planes in image (must be 1)
+  unsigned short int bpp;             // number of bits per pixel (must be 24)
+  char temp;                          // temporary color storage for bgr-rgb conversion.
+
+  // make sure the file is there.
+  if ((file = fopen(filename.c_str(), "rb"))==NULL)
+  {
+    printf("File Not Found : %s\n",filename.c_str());
+    return 0;
+  }
+
+  // seek through the bmp header, up to the width/height:
+  fseek(file, 18, SEEK_CUR);
+
+  // read the width
+  if ((i = fread(&image->sizeX, 4, 1, file)) != 1) {
+    printf("Error reading width from %s.\n", filename.c_str());
+    return 0;
+  }
+  printf("Width of %s: %lu\n", filename.c_str(), image->sizeX);
+
+  // read the height
+  if ((i = fread(&image->sizeY, 4, 1, file)) != 1) {
+    printf("Error reading height from %s.\n", filename.c_str());
+    return 0;
+  }
+  printf("Height of %s: %lu\n", filename.c_str(), image->sizeY);
+
+  // calculate the size (assuming 24 bits or 3 bytes per pixel).
+  size = image->sizeX * image->sizeY * 3;
+
+  // read the planes
+  if ((fread(&planes, 2, 1, file)) != 1) {
+    printf("Error reading planes from %s.\n", filename.c_str());
+    return 0;
+  }
+  if (planes != 1) {
+    printf("Planes from %s is not 1: %u\n", filename.c_str(), planes);
+    return 0;
+  }
+
+  // read the bpp
+  if ((i = fread(&bpp, 2, 1, file)) != 1) {
+    printf("Error reading bpp from %s.\n", filename.c_str());
+    return 0;
+  }
+  if (bpp != 24) {
+    printf("Bpp from %s is not 24: %u\n", filename.c_str(), bpp);
+    return 0;
+  }
+
+  // seek past the rest of the bitmap header.
+  fseek(file, 24, SEEK_CUR);
+
+  // read the data.
+  image->data = (char *) malloc(size);
+  if (image->data == NULL) {
+    printf("Error allocating memory for color-corrected image data");
+    return 0;
+  }
+
+  if ((i = fread(image->data, size, 1, file)) != 1) {
+    printf("Error reading image data from %s.\n", filename.c_str());
+    return 0;
+  }
+
+  for (i=0;i<size;i+=3) { // reverse all of the colors. (bgr -> rgb)
+    temp = image->data[i];
+    image->data[i] = image->data[i+2];
+    image->data[i+2] = temp;
+  }
+
+  // we're done.
+  return 1;
+}
+
+// Load Bitmaps And Convert To Textures
+bool LoadGLTextures() {
+  // Load Texture
+  Image *image1;
+
+  // allocate space for texture
+  image1 = (Image *) malloc(sizeof(Image));
+  if (image1 == NULL) {
+    printf("Error allocating space for image");
+    exit(0);
+  }
+
+  if (!ImageLoad("tex.bmp", image1)) {
+    exit(1);
+  }
+
+  // Create Texture
+  glGenTextures(1, &texture[0]);
+  glBindTexture(GL_TEXTURE_2D, texture[0]);   // 2d texture (x and y size)
+
+  glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); // scale linearly when image bigger than texture
+  glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); // scale linearly when image smalled than texture
+
+  // 2d texture, level of detail 0 (normal), 3 components (red, green, blue), x size from image, y size from image,
+  // border 0 (normal), rgb color data, unsigned byte data, and finally the data itself.
+  glTexImage2D(GL_TEXTURE_2D, 0, 3, image1->sizeX, image1->sizeY, 0, GL_RGB, GL_UNSIGNED_BYTE, image1->data);
+  return true;
+}
+
 /* A general OpenGL initialization function.  Sets all of the initial parameters. */
-void InitGL(int Width, int Height)	        // We call this right after our OpenGL window is created.
+bool InitGL(int Width, int Height)	        // We call this right after our OpenGL window is created.
 {
   glClearColor(0.0f, 0.0f, 0.0f, 0.0f);		// This Will Clear The Background Color To Black
   glClearDepth(1.0);				// Enables Clearing Of The Depth Buffer
-  glDepthFunc(GL_LESS);				// The Type Of Depth Test To Do
+  //glDepthFunc(GL_LESS);				// The Type Of Depth Test To Do
   glEnable(GL_DEPTH_TEST);			// Enables Depth Testing
   glShadeModel(GL_SMOOTH);			// Enables Smooth Color Shading
 
@@ -44,6 +168,16 @@ void InitGL(int Width, int Height)	        // We call this right after our OpenG
   gluPerspective(90.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);	// Calculate The Aspect Ratio Of The Window
 
   glMatrixMode(GL_MODELVIEW);
+
+  if (!LoadGLTextures()) {// Nahraje texturu
+    return false;
+  }
+
+  glEnable(GL_TEXTURE_2D);// Zapne mapování textur
+  glDepthFunc(GL_LEQUAL);// Typ hloubkového testování
+  glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);// Nejlepší perspektivní korekce
+
+  return true;
 }
 
 /* The function called when our window is resized (which shouldn't happen, because we're fullscreen) */
@@ -71,6 +205,7 @@ void DrawGLScene()
   glTranslatef(-15.0f,-15.0f,-15.0f);
 
   //voxel::drawToe(clr);
+  glBindTexture(GL_TEXTURE_2D, texture[0]);
   ch.draw(clr);
 
   // since this is double buffered, swap the buffers to display what just got drawn.
